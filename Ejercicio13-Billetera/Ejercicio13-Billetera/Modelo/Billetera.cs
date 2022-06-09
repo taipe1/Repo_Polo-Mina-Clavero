63970967f0cde368c9bb29e47a5a364f37dcc62e
Andy Taipe <ataipe@unitru.edu.pe>
2022-06-09 02:21:36 -0500

    Ejercicio Billetera terminado

---
+++ Ejercicio13-Billetera/Ejercicio13-Billetera/Modelo/Billetera.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13_Billetera.Modelo
+{
+    public class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+        public decimal Total()
+        {
+            return BilletesDe10 * 10
+                + BilletesDe20 * 20
+                + BilletesDe50 * 50
+                + BilletesDe100 * 100
+                + BilletesDe200 * 200
+                + BilletesDe500 * 500
+                + BilletesDe1000 * 1000;
+        }
+        public static Billetera Combinar(Billetera miBilletera, Billetera otraBilletera, Billetera billeteraCombinada)
+        {
+            billeteraCombinada.BilletesDe10 = miBilletera.BilletesDe10 + otraBilletera.BilletesDe10;
+            billeteraCombinada.BilletesDe20 = miBilletera.BilletesDe20 + otraBilletera.BilletesDe20;
+            billeteraCombinada.BilletesDe50 = miBilletera.BilletesDe50 + otraBilletera.BilletesDe50;
+            billeteraCombinada.BilletesDe100 = miBilletera.BilletesDe100 + otraBilletera.BilletesDe100;
+            billeteraCombinada.BilletesDe200 = miBilletera.BilletesDe200 + otraBilletera.BilletesDe200;
+            billeteraCombinada.BilletesDe500 = miBilletera.BilletesDe500 + otraBilletera.BilletesDe500;
+            billeteraCombinada.BilletesDe1000 = miBilletera.BilletesDe1000 + otraBilletera.BilletesDe1000;
+
+            miBilletera.BilletesDe10 = 0;
+            miBilletera.BilletesDe20 = 0;
+            miBilletera.BilletesDe50 = 0;
+            miBilletera.BilletesDe100 = 0;
+            miBilletera.BilletesDe200 = 0;
+            miBilletera.BilletesDe500 = 0;
+            miBilletera.BilletesDe1000 = 0;
+
+            otraBilletera.BilletesDe10 = 0;
+            otraBilletera.BilletesDe20 = 0;
+            otraBilletera.BilletesDe50 = 0;
+            otraBilletera.BilletesDe100 = 0;
+            otraBilletera.BilletesDe200 = 0;
+            otraBilletera.BilletesDe500 = 0;
+            otraBilletera.BilletesDe1000 = 0;
+
+            return billeteraCombinada;
+        }
+
+    }
+
+
+}
---
+++ Ejercicio13-Billetera/Ejercicio13-Billetera/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Ejercicio13_Billetera.Modelo;
+
+Billetera miBilletera = new();
+PedirDatos(miBilletera);
+Console.WriteLine($"Mi billetera tiene: {miBilletera.Total()} ");
+
+Billetera otraBilletera = new();
+PedirDatos(otraBilletera);
+Console.WriteLine($"La otra billetera tiene: {otraBilletera.Total()} ");
+
+Billetera billteraCombinada = new();
+Billetera.Combinar(miBilletera, otraBilletera, billteraCombinada);
+
+Console.WriteLine($"La billetera combinada tiene: {billteraCombinada.Total()} ");
+Console.WriteLine($"Mi billetera ahora tiene {miBilletera.Total()}");
+Console.WriteLine($"La otra billetera ahora tiene {otraBilletera.Total()}");
+
+void PedirDatos(Billetera billetera)
+{
+    Console.WriteLine("Ingrese cuántos billetes de 10 soles tienes");
+    billetera.BilletesDe10 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese cuántos billetes de 20 soles tienes");
+    billetera.BilletesDe20 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese cuántos billetes de 50 tienes");
+    billetera.BilletesDe50 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese cuántos billetes de 100 soles queres?");
+    billetera.BilletesDe100 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese cuántos billetes de 200 soles queres?");
+    billetera.BilletesDe200 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese cuántos billetes de 500 soles queres?");
+    billetera.BilletesDe500 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese cuántos billetes de 1000 soles queres?");
+    billetera.BilletesDe1000 = int.Parse(Console.ReadLine());
+}
